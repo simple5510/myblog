6bbea24a9b04eb829a8eff0d3474d3597223e417
chunyangchi@gmail.com <chunyangchi@gmail.com>
2018-12-27 18:38:39 +0800

    初步实现点赞功能

--- myblog/settings.py
+++ myblog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'read_statistics',
     'comment',
+    'like',
 ]
 
 MIDDLEWARE = [
--- myblog/urls.py
+++ myblog/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('blog/', include('blog.urls')),
     path('comment/', include('comment.urls')),
+    path('like/', include('like.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
